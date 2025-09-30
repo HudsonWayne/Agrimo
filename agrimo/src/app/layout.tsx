b5ef56c999ebf544c7b2f023350203bc8b709ab8
HudsonWayne <waynewaynebenhura@gmail.com>
2025-09-30 10:59:50 +0200

    initial commit

--- agrimo/src/app/layout.tsx
+++ agrimo/src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Navbar from "@/components/Navbar"; // 👈 import your Navbar
+// 👈 import your Navbar
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,10 +28,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {/* ✅ Navbar on every page */}
-        <Navbar />
 
-        {/* Page content */}
         <main>{children}</main>
       </body>
     </html>
